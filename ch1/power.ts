b27b84a701dd7e88f0a194bbd46570c2a8bea406
Wycliffe-Kim <80871550+Wycliffe-Kim@users.noreply.github.com>
2021-12-06 20:13:34 +0900

    OOP and FP average function comparison (#3)

--- ch1/power.ts
+++ ch1/power.ts
@@ -1,13 +1,14 @@
+const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 function power_oop() {
-  const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   for (let i = 0; i < array.length; i++) {
     array[i] = Math.pow(array[i], 2);
   }
 
-  console.log('oopPower:', array);
+  console.log('oopPower', array);
 }
 
-const power_fp = () => console.log('fpPower', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((num) => Math.pow(num, 2)));
+const power_fp = () => console.log('fpPower', array.map((num) => Math.pow(num, 2)));
 
 export {
   power_oop,
--- ch1/showStudent.ts
+++ ch1/showStudent.ts
@@ -1,18 +1,17 @@
 import { studentDB } from './db';
 import fp from 'lodash/fp';
 
+const ssn = '444-44-4444';
 function showStudent_oop() {
-  const ssn = '444-44-4444';
   let student = studentDB.find(ssn);
   if (student != null) {
-    console.log(`${student.ssn}, ${student.firstname}, ${student.lastname}`);
+    console.log('showStudent_oop', `${student.ssn}, ${student.firstname}, ${student.lastname}`);
   } else {
     throw new Error('학생을 찾을 수 없습니다.');
   }
 }
 
 function showStudent_fp() {
-  const ssn = '444-44-4444';
   const findFrom = fp.curry((db: any, id: string) => {
     let obj = db.find(id);
     if (obj == null) {
@@ -23,7 +22,7 @@ function showStudent_fp() {
   });
 
   const makeString = (student: any) => `${student.ssn}, ${student.firstname}, ${student.lastname}`;
-  const log = (info: string) => console.log(info);
+  const log = (info: string) => console.log('showStudent_fp', info);
 
   const showStudent = fp.compose(
     log,
