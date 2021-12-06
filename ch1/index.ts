6e35b0141edde2e088b84b330756788f173b28a1
김원동 <wycliffe@gim-wondong-ui-MacBookAir.local>
2021-12-07 08:08:25 +0900

    OOP and FP average grade comparison

--- ch1/index.ts
+++ ch1/index.ts
@@ -1,4 +1,4 @@
-import { average_fp, average_oop } from './average';
+import { averageGrade_fp, averageGrade_oop, average_fp, average_oop } from './average';
 import { power_oop, power_fp } from './power';
 import { showStudent_fp, showStudent_oop } from './showStudent';
 
@@ -8,7 +8,9 @@ const codes = [
   showStudent_oop,
   showStudent_fp,
   average_oop,
-  average_fp
+  average_fp,
+  averageGrade_oop,
+  averageGrade_fp
 ];
 
 const main = () => codes.forEach(code => code());
