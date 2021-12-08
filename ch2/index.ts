64c026bdd8442c1521d4f720ec88b59e1ecbeaed
김원동 <wycliffe@gim-wondong-ui-MacBookAir.local>
2021-12-08 18:25:43 +0900

    OOP and FP reduce function comparison

--- ch2/index.ts
+++ ch2/index.ts
@@ -1,10 +1,13 @@
 import main from '../main';
 import { findStudentsBy_fp, findStudentsBy_oop } from './findStudentsBy';
 import { map_fp, map_oop } from './map';
+import { reduce_fp, reduce_oop } from './reduce';
 
 main([
   findStudentsBy_oop,
   findStudentsBy_fp,
   map_oop,
-  map_fp
+  map_fp,
+  reduce_oop,
+  reduce_fp
 ]);
