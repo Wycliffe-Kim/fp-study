50b7e149b8394c4ce451b4fdc6d1ef1a1ec3dbad
Wycliffe-Kim <80871550+Wycliffe-Kim@users.noreply.github.com>
2021-12-08 18:54:12 +0900

    OOP and FP sort function comparison (#12)

--- ch3/index.ts
+++ ch3/index.ts
@@ -2,6 +2,7 @@ import main from '../main';
 import { filter_fp, filter_oop } from './filter';
 import { map_fp, map_oop } from './map';
 import { reduce_fp, reduce_oop } from './reduce';
+import { sort_fp, sort_oop } from './sort';
 
 main([
   map_oop,
@@ -9,5 +10,7 @@ main([
   reduce_oop,
   reduce_fp,
   filter_oop,
-  filter_fp
+  filter_fp,
+  sort_oop,
+  sort_fp
 ]);
