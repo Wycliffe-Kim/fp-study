835e750714b1e0a040c752c00cf23bdb4a7808ad
Wycliffe-Kim <80871550+Wycliffe-Kim@users.noreply.github.com>
2021-12-10 16:15:46 +0900

    change compose ts (#29)

--- typescript/ch4/compose.ts
+++ typescript/ch4/compose.ts
@@ -1,4 +1,4 @@
-import R from 'ramda';
+import fp from 'lodash/fp';
 
 function compose() {
   const str = 'We can only see a short distance ' +
@@ -7,8 +7,19 @@ function compose() {
 
   const explode = (str: string) => str.split(/\s+/);
   const count = (arr: string[]) => arr.length;
-  const countWords = R.compose(count, explode);
+  const countWords = fp.compose(count, explode);
   console.log(countWords(str));
+
+  const students = ['Rosser', 'Turing', 'Kleene', 'Church'];
+  const grades = [80, 100, 90, 99];
+  const smartestStudent = fp.compose(
+    (data: any) => data[0],
+    fp.head,
+    fp.reverse,
+    fp.sortBy(fp.prop(1)),
+    fp.zip
+  );
+  console.log(smartestStudent(students, grades));
 }
 
 export {
