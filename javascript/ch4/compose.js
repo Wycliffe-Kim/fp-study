4f211c824294c6c2ff4ccd72afaf8a066b2f4e62
김원동 <wycliffe@gim-wondong-ui-MacBookAir.local>
2021-12-10 16:17:01 +0900

    change compose js

--- javascript/ch4/compose.js
+++ javascript/ch4/compose.js
@@ -1,4 +1,4 @@
-const R = require('ramda');
+const fp = require('lodash/fp');
 
 function compose() {
   const str = 'We can only see a short distance ' +
@@ -7,8 +7,19 @@ function compose() {
 
   const explode = (str) => str.split(/\s+/);
   const count = (arr) => arr.length;
-  const countWords = R.compose(count, explode);
+  const countWords = fp.compose(count, explode);
   console.log(countWords(str));
+
+  const students = ['Rosser', 'Turing', 'Kleene', 'Church'];
+  const grades = [80, 100, 90, 99];
+  const smartestStudent = fp.compose(
+    (data) => data[0],
+    fp.head,
+    fp.reverse,
+    fp.sortBy(fp.prop(1)),
+    fp.zip
+  );
+  console.log(smartestStudent(students, grades));
 }
 
 module.exports = {
