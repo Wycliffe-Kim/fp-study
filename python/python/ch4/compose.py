3911c14647282e47975fde55a95a59a7a5fa9cc2
김원동 <wycliffe@gim-wondong-ui-MacBookAir.local>
2021-12-10 16:38:15 +0900

    change compose python

--- python/python/ch4/compose.py
+++ python/python/ch4/compose.py
@@ -1,5 +1,7 @@
 import re
 import toolz as tz
+import pydash
+import operator
 
 def compose():
     s = str('We can only see a short distance '
@@ -11,3 +13,14 @@ def compose():
     count = lambda arr: len(arr)
     count_words = tz.compose(count, explode)
     print(count_words(s))
+    
+    students = ['Rosser', 'Turing', 'Kleene', 'Church']
+    grades = [80, 100, 90, 99]
+    smartest_student = tz.compose(
+        lambda data: data[0],
+        pydash.head,
+        pydash.reverse,
+        lambda arr: sorted(arr, key=operator.itemgetter(1)),
+        pydash.zip_)
+    
+    print(smartest_student(students, grades))
