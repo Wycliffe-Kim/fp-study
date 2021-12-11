fad13e09e0ca89237515480154a3bdbd44fcf797
Wycliffe-Kim <80871550+Wycliffe-Kim@users.noreply.github.com>
2021-12-11 11:31:58 +0900

    arrange compose python (#37)

--- python/python/ch4/compose.py
+++ python/python/ch4/compose.py
@@ -3,7 +3,7 @@ import toolz as tz
 import pydash
 import operator
 
-def compose():
+def compose1():
     s = str('We can only see a short distance '
             'ahead but we can see plenty there '
             'that needs to be done')
@@ -14,6 +14,7 @@ def compose():
     count_words = tz.compose(count, explode)
     print(count_words(s))
     
+def compose2():
     students = ['Rosser', 'Turing', 'Kleene', 'Church']
     grades = [80, 100, 90, 99]
     smartest_student = tz.compose(
@@ -24,3 +25,15 @@ def compose():
         pydash.zip_)
     
     print(smartest_student(students, grades))
+    
+def compose3():
+    trim = lambda str: re.search('^\s*|\s*$', str).string
+    normalize = lambda str: ''.join(re.split('\-', str))
+    valid_length = lambda length, str: len(str) == length
+    check_length_ssn = pydash.partial(valid_length, 9)
+    
+    clean_input = tz.compose(normalize, trim)
+    is_valid_ssn = tz.compose(check_length_ssn, clean_input)
+    
+    print(clean_input('444-44-4444'))
+    print(is_valid_ssn('444-44-4444'))
