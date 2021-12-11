6908ae86154bbe74de083c384d6436de1cc5b8aa
Wycliffe-Kim <80871550+Wycliffe-Kim@users.noreply.github.com>
2021-12-11 11:50:24 +0900

    seperate between impure and pure ts (#38)

--- typescript/ch4/index.ts
+++ typescript/ch4/index.ts
@@ -2,11 +2,13 @@ import main from '../main';
 import { currying } from './currying';
 import { compose1, compose2, compose3 } from './compose';
 import { partial } from './partial';
+import { impureAndPure } from './impureAndPure';
 
 main([
   currying,
   compose1,
   compose2,
   compose3,
-  partial
+  partial,
+  impureAndPure
 ]);
---
+++ typescript/ch4/normalize.ts
@@ -0,0 +1,5 @@
+const normalize = (str: string) => str.replace(/\-/g, '');
+
+export {
+  normalize
+};
