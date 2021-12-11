38a3c65532efd7e8d8f6219cf321ff99581638b4
Wycliffe-Kim <80871550+Wycliffe-Kim@users.noreply.github.com>
2021-12-11 11:02:58 +0900

    partial js (#33)

--- javascript/ch4/index.js
+++ javascript/ch4/index.js
@@ -1,8 +1,10 @@
 const { main } = require('../main');
 const { currying } = require('./currying');
 const { compose } = require('./compose');
+const { partial } = require('./partial');
 
 main([
   currying,
-  compose
+  compose,
+  partial
 ]);
