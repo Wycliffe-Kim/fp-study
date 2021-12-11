4b19ec9b7583fc3ec9f4403fc37ae28ea506fe13
김원동 <wycliffe@gim-wondong-ui-MacBookAir.local>
2021-12-11 11:31:36 +0900

    arrange compose python

--- python/python/ch4/index.py
+++ python/python/ch4/index.py
@@ -1,10 +1,12 @@
 from ..main import main
 from .currying import currying
-from .compose import compose
+from .compose import compose1, compose2, compose3
 from .partial import partial
 
 index = lambda: main([
         currying,
-        compose,
+        compose1,
+        compose2,
+        compose3,
         partial
     ])
