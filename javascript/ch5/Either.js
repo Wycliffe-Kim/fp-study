72b389f20efe128d778db20492a900dee59b9377
Wycliffe-Kim <80871550+Wycliffe-Kim@users.noreply.github.com>
2021-12-20 07:26:56 +0900

    change monad js (#49)

--- javascript/ch5/Either.js
+++ javascript/ch5/Either.js
@@ -6,8 +6,90 @@ class Either {
   get value() {
     return this._value;
   }
+
+  static left(value) {
+    return new Left(value);
+  }
+
+  static right(value) {
+    return new Right(value);
+  }
+
+  static fromNullable(value) {
+    return value != null ? Either.right(value) : Either.left(value);
+  }
+
+  static of(value) {
+    return Either.right(value);
+  }
+}
+
+class Left extends Either {
+  get value() {
+    throw TypeError(`Left(${this._value}) 값을 가져올 수 없습니다.`);
+  }
+
+  map(f) {
+    return this;
+  }
+
+  getOrElse(other) {
+    return other
+  }
+
+  orElse(f) {
+    return f(this._value);
+  }
+
+  chain(f) {
+    return this;
+  }
+
+  getOrElseThrow(value) {
+    return new Error(value);
+  }
+
+  filter(f) {
+    return this;
+  }
+
+  toString() {
+    return `Either.Left(${this._value})`;
+  }
+}
+
+class Right extends Either {
+  map(f) {
+    return Either.fromNullable(f(this._value));
+  }
+
+  getOrElse(other) {
+    return this._value;
+  }
+
+  orElse() {
+    return this;
+  }
+
+  chain(f) {
+    return f(this._value);
+  }
+
+  getOrElseThrow() {
+    return this._value;
+  }
+
+  filter(f) {
+    return Either.fromNullable(f(this._value) ? this._value : null);
+  }
+
+  toString() {
+    return `Either.Right(${this._value})`;
+  }
 }
 
 module.exports = {
-  Either
+  Either,
+  Left,
+  Right
 };
--- javascript/ch5/Maybe.js
+++ javascript/ch5/Maybe.js
@@ -6,8 +6,91 @@ class Maybe {
   get isJust() {
     return false;
   }
+
+  static just(value) {
+    return new Just(value);
+  }
+
+  static nothing() {
+    return new Nothing();
+  }
+
+  static fromNullable(value) {
+    return value != null ? Maybe.just(value) : Maybe.nothing();
+  }
+
+  static of(value) {
+    return Maybe.just(value);
+  }
+}
+
+class Nothing extends Maybe {
+  map(f) {
+    return this;
+  }
+
+  get value() {
+    throw new TypeError('Nothing 값을 가져올 수 없습니다.');
+  }
+
+  getOrElse(other) {
+    return other;
+  }
+
+  filter(f) {
+    return this._value;
+  }
+
+  chain(f) {
+    return this;
+  }
+
+  toString() {
+    return 'Maybe.Nothing';
+  }
+
+  get isNothing() {
+    return true;
+  }
+}
+
+class Just extends Maybe {
+  constructor(value) {
+    super();
+    this._value = value;
+  }
+
+  get value() {
+    return this._value;
+  }
+
+  map(f) {
+    return Maybe.fromNullable(f(this._value));
+  }
+
+  getOrElse(other) {
+    return this._value;
+  }
+
+  filter(f) {
+    Maybe.fromNullable(f(this._value) ? this._value : null);
+  }
+
+  chain(f) {
+    return f(this._value);
+  }
+
+  toString() {
+    return `Maybe.Just(${this._value})`;
+  }
+
+  get isJust() {
+    return true;
+  }
 }
 
 module.exports = {
-  Maybe
+  Maybe,
+  Nothing,
+  Just
 };
