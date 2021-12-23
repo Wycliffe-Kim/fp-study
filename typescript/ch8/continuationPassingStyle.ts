daf1f15bacc34c85b34dd79c5e1ca5f914014f16
Wycliffe-Kim <80871550+Wycliffe-Kim@users.noreply.github.com>
2021-12-23 16:57:51 +0900

    correct continuation passing style ts (#73)

--- typescript/ch8/continuationPassingStyle.ts
+++ typescript/ch8/continuationPassingStyle.ts
@@ -1,16 +1,22 @@
+import R from 'ramda';
+import { IO } from '../ch5/IO';
+import average from './average';
 import { getJSONCallback } from './getJSON';
 
 // CPS(Continuation Passing Style)은 이와 같이 콜백 지옥과 비교해 보았을 때,
 // 함수를 별개의 컴포넌트로 빼는 차이밖에 없지만 스택 효율이 더 좋다.
 function continuationPassingStyle() {
+  const log = R.curry((title: string, message: any) => console.log(title, message));
+
   const success = (data: any) => {
-    console.log('continuationPassingStyle', 'success');
-    console.log(data);
+    const grades = R.map((student: any) => student.grade)(data);
+    const grade = average(grades);
+    const io = (grade: number) => IO.of(grade).map(log('continuationPassingStyle success')).run();
+    io(grade);
   }
 
-  const fail = (error: Error) => {
-    console.log('continuationPassingStyle', 'fail');
-    console.log(error);
+  const fail = (err: Error) => {
+    log('callbackHell fail', err);
   }
 
   getJSONCallback('http://localhost:5555/students', success, fail);
