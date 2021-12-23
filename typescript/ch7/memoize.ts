ce0319f29f0475b12878f67c1e81a3cbb538cf76
Wycliffe-Kim <80871550+Wycliffe-Kim@users.noreply.github.com>
2021-12-23 09:58:08 +0900

    check performance ts (#64)

--- typescript/ch7/memoize.ts
+++ typescript/ch7/memoize.ts
@@ -1,6 +1,6 @@
 import fp from 'lodash/fp';
 import _ from 'lodash';
-import { logChecker, mapBetweenChecker, startChecker, stopChecker } from './PerformanceChecker';
+import checkPerformance from './checkPerformance';
 
 function memoize() {
   // FP 에서는 이런 식으로 외부 변수가 있으면 안 되지만 테스트 결과를 위해서 임시로 사용한다..
@@ -13,17 +13,16 @@ function memoize() {
 
   const logCount = () => console.log(count);
   const resetCount = () => { count = 0; }
-  const checkPerformance = (name: string, func: any) => fp.compose(
+  const loop = (func: any) => _.range(10000000).forEach(func);
+  const checkPerformancePlusCount = (name: string, func: any) => fp.compose(
     logCount,
-    logChecker('ms'),
-    stopChecker,
-    mapBetweenChecker(() => _.range(10000000).forEach(func)),
-    startChecker(false, 0),
-    fp.tap(resetCount)
-  )(name);
+    () => checkPerformance(name, func),
+    resetCount
+  )();
 
-  checkPerformance('add', () => add(1, 2));
-  checkPerformance('addMemoized', () => addMemoized(1, 2));
+  checkPerformancePlusCount('add', () => loop(() => add(1, 2)));
+  checkPerformancePlusCount('addMemoized', () => loop(() => addMemoized(1, 2)));
+  
 }
 
 export {
