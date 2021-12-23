8b2e1b0bb47d93fca374e44fd117b12f10f331a7
Wycliffe-Kim <80871550+Wycliffe-Kim@users.noreply.github.com>
2021-12-23 16:54:31 +0900

    correct promise ts (#72)

--- typescript/ch8/promise.ts
+++ typescript/ch8/promise.ts
@@ -4,11 +4,13 @@ import average from './average';
 import { getJSONPromise } from './getJSON';
 
 function promise() {
+  const log = R.curry((title: string, message: any) => console.log(title, message));
+
   getJSONPromise('http://localhost:5555/students')
   .then(R.map((student: any) => student.grade))
   .then(average)
-  .then((grade) => IO.of(grade).map(console.log).run())
-  .catch((error) => console.log('promise fail', error));
+  .then((grade) => IO.of(grade).map(log('promise success')).run())
+  .catch((error) => log('promise fail', error));
 }
 
 export {
