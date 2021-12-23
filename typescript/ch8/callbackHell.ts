3badc9c8fb4a1a563e3d4fa28d4475d302e8ef60
Wycliffe-Kim <80871550+Wycliffe-Kim@users.noreply.github.com>
2021-12-23 16:53:29 +0900

    correct callback hell ts (#71)

--- typescript/ch8/callbackHell.ts
+++ typescript/ch8/callbackHell.ts
@@ -1,12 +1,18 @@
+import R from 'ramda';
+import { IO } from '../ch5/IO';
+import average from './average';
 import { getJSONCallback } from './getJSON';
 
 function callbackHell() {
+  const log = R.curry((title: string, message: any) => console.log(title, message));
+
   getJSONCallback('http://localhost:5555/students', (data) => {
-    console.log('callbackHell', 'success');
-    console.log(data);
+    const grades = R.map((student: any) => student.grade)(data);
+    const grade = average(grades);
+    const io = (grade: number) => IO.of(grade).map(log('callbackHell success')).run();
+    io(grade);
   }, (err) => {
-    console.log('callbackHell', 'fail');
-    console.log(err);
+    log('callbackHell fail', err);
   });
 }
 
