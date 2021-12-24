d6f7a4b29c2909eb01e5c86b5b1e2f20b220ad19
Wycliffe-Kim <80871550+Wycliffe-Kim@users.noreply.github.com>
2021-12-24 16:35:54 +0900

    recursive generator js (#84)

--- javascript/ch8/index.js
+++ javascript/ch8/index.js
@@ -3,10 +3,12 @@ const callbackHell = require('./callbackHell');
 const continuationPassingStyle = require('./continuationPassingStyle');
 const promise = require('./promise');
 const lazyCreateData = require('./lazyCreateData');
+const recursiveGenerator = require('./recursiveGenerator');
 
 main([
   callbackHell,
   continuationPassingStyle,
   promise,
   ...lazyCreateData,
+  ...recursiveGenerator,
 ]);
