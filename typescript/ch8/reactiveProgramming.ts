413490a41a47c06318f41cd7f24f7db3dabc6cb6
Wycliffe-Kim <80871550+Wycliffe-Kim@users.noreply.github.com>
2021-12-24 15:33:43 +0900

    reactive programming ts (#77)

---
+++ typescript/ch8/reactiveProgramming.ts
@@ -0,0 +1,63 @@
+import _ from 'lodash';
+import R from 'ramda'
+import { Observable, of, from } from 'rxjs';
+import { map, filter, tap } from 'rxjs/operators';
+
+import average from './average';
+import { getJSONPromise } from './getJSON';
+
+const context = 'reactiveProgramming';
+const startContextLogging = (index: number) => console.log(`${context}${index}`);
+
+const reactiveProgramming = [
+  () => {
+    startContextLogging(1);
+
+    const observable = new Observable((subscriber) => {
+      _.range(1, 10).forEach((i) => subscriber.next(i));
+      subscriber.complete();
+    });
+
+    observable.subscribe({
+      next(x) { console.log('next', x); },
+      complete() { console.log('complete'); }
+    });
+  },
+
+  () => {
+    startContextLogging(2);
+    
+    const squares = (amount: number) => 
+      new Observable((subscriber) => 
+        _.range(1, amount + 1).forEach((i) => subscriber.next(i * i)));
+  
+    squares(5).subscribe((x) => console.log(`다음: ${x}`));
+  },
+
+  () => {
+    startContextLogging(3);
+
+    of(1, 2, 3, 4, 5)
+    .pipe(
+      filter((x) => x % 2 !== 0),
+      map((x) => x * x)
+    )
+    .subscribe((x) => console.log(`다음: ${x}`));
+  },
+
+  () => {
+    startContextLogging(4);
+
+    from(getJSONPromise('http://localhost:5555/students'))
+    .pipe(
+      map(R.map((student: any) => student.grade)),
+      map(average),
+    )
+    .subscribe({
+      next(student) { console.log(student) },
+      error(err) { console.log(err) }
+    });
+  }
+];
+
+export default reactiveProgramming;
