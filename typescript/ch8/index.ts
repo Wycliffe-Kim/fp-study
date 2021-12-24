a67c51f915bff2fac508dd42e20177d3609b71c7
Wycliffe-Kim <80871550+Wycliffe-Kim@users.noreply.github.com>
2021-12-24 11:21:36 +0900

    lazy create data ts (#74)

--- typescript/ch8/index.ts
+++ typescript/ch8/index.ts
@@ -1,10 +1,14 @@
 import main from '../main';
 import { callbackHell } from './callbackHell';
 import { continuationPassingStyle } from './continuationPassingStyle';
+import { lazyCreateData1, lazyCreateData2, lazyCreateData3 } from './lazyCreateData';
 import { promise } from './promise';
 
 main([
   callbackHell,
   continuationPassingStyle,
-  promise
+  promise,
+  lazyCreateData1,
+  lazyCreateData2,
+  lazyCreateData3
 ]);
