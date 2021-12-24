860b6eddbc6d9feb0189b933afbb44ef5dc1c8f5
김원동 <wycliffe@gim-wondong-ui-MacBookAir.local>
2021-12-24 12:57:57 +0900

    recursive generator ts

--- typescript/ch8/index.ts
+++ typescript/ch8/index.ts
@@ -3,6 +3,7 @@ import { callbackHell } from './callbackHell';
 import { continuationPassingStyle } from './continuationPassingStyle';
 import { lazyCreateData1, lazyCreateData2, lazyCreateData3 } from './lazyCreateData';
 import { promise } from './promise';
+import { recursiveGenerator1, recursiveGenerator2 } from './recursiveGenerator';
 
 main([
   callbackHell,
@@ -10,5 +11,7 @@ main([
   promise,
   lazyCreateData1,
   lazyCreateData2,
-  lazyCreateData3
+  lazyCreateData3,
+  recursiveGenerator1,
+  recursiveGenerator2
 ]);
