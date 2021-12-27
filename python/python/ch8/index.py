94997070570b7a47a2fe5531d3a5b22d9b3bf671
Wycliffe-Kim <80871550+Wycliffe-Kim@users.noreply.github.com>
2021-12-27 12:09:04 +0900

    recursive generator python (#91)

--- python/python/ch8/index.py
+++ python/python/ch8/index.py
@@ -2,9 +2,11 @@ from ..main import main
 from .callback_hell import callback_hell
 from .promise import promise
 from .lazy_create_data import lazy_create_data
+from .recursive_generator import recursive_generator
 
 index = lambda: main([
         callback_hell,
         promise,
-        *lazy_create_data
+        *lazy_create_data,
+        *recursive_generator
     ])
